868e569613d291ce2d717dd56aa28966b7e9a5c4
jrfm <moesch.jonas@protonmail.com>
2022-08-25 15:45:54 +0200

    add basic layout

---
+++ index.js
@@ -0,0 +1,24 @@
+const GameBoard = (gameBoadArray) => {
+    console.log('I am the gameboard')
+
+};
+
+const activeGameBoard = GameBoard([1,2]);
+
+
+const createGameBoard = () => {
+    // creates gameboard
+    
+    const logBoard = ()=> console.log('create game board');
+    const createBoard = () => {
+        let i = 0;
+        while ( i < 10) {
+            console.log(i)
+        };
+    };
+    return 
+}
+
+
+const newBoard = createGameBoard();
+newBoard();
